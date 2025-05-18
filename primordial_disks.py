f9d825283b44966a55b62fdad702bb10ee761912
Lauren <51422323+lbiddle@users.noreply.github.com>
2025-05-18 10:17:07 -0700

    Add files via upload

---
+++ primordial_disks.py
@@ -0,0 +1,21 @@
+import numpy as np
+import sys
+sys.path.append('/Users/lib278/opt/anaconda3/envs/pyKLIP/lib/python3.9/site-packages/')
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    bep = 4
+
+
+
+
